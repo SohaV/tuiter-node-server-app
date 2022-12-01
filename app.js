9ebaa36d98b782db0c95c8eda5aac361b40ef74c
SOHA-VANKUDRE\vanku <vankudresoha12@gmail.com>
2022-12-01 18:03:52 -0500

    A9 changes

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
 import mongoose from "mongoose";
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-//     || 'mongodb://localhost:27017/tuiter';
-const CONNECTION_STRING = 'mongodb+srv://SohaVankudre:Soha7120&@cluster0.jctuepg.mongodb.net/tuiter?retryWrites=true&w=majority'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/tuiter';
+// const CONNECTION_STRING = 'mongodb+srv://SohaVankudre:Soha7120&@cluster0.jctuepg.mongodb.net/tuiter?retryWrites=true&w=majority'
+//     || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 
 // mongoose.connect('mongodb://localhost:27017/tuiter');
