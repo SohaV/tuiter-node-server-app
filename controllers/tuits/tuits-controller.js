a950aae80594ad2cdf1bca66311b183bb509fc3d
SOHA-VANKUDRE\vanku <vankudresoha12@gmail.com>
2022-12-01 18:42:04 -0500

    A9 changes

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -4,7 +4,7 @@ let tuits = posts;
 const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime()+'';
-    newTuit.likes = 1;
+    newTuit.likes = 0;
     newTuit.dislikes = 0;
     newTuit.replies = 0;
     newTuit.retuits = 0;
@@ -14,7 +14,7 @@ const createTuit = (req, res) => {
     newTuit.topic = 'Sample Topic';
     newTuit.username = 'Sample Username';
     newTuit.title = 'Sample Title';
-    newTuit.liked = true;
+    newTuit.liked = false;
     tuits.push(newTuit);
     res.json(newTuit);
 }
