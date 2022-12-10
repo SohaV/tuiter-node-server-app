bc3948a9cc6cb0ea08b571f26d58e21c4adb471c
SOHA-VANKUDRE\vanku <vankudresoha12@gmail.com>
2022-12-10 13:20:38 -0500

    A9 changes

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
-const schema = mongoose.Schema({
+const schema = new mongoose.Schema({
     tuit: {type: String, default: "This is a tuit test"},
     likes: Number,
     liked: Boolean,
-}, {collection: "tuits"});
+    handle: {type: String, default: "@testHandle"},
+    image: {type: String, default: "../../images/spacex.jpg"},
+    topic: {type: String, default: "Sample Title"},
+    time: {type: String, default: "2h"},
+    dislikes: {type: Number, default: 0},
+    replies: {type: Number, default: 0},
+    retuits: {type: Number, default: 0},
+    userName: {type: String, default: 'sampleUserName'}
+}, {collection: 'tuits'});
 export default schema;
